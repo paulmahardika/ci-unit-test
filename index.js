81a8c871ef9f91c14f5106e5a2fbcb4ed57eade0
paulusmahardika <paulusmahardika@refactory.id>
2024-02-27 18:38:34 +0700

    Update CI - failed case

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const calculate = (a, b) => {
   return a + b;
 };
 
+const calculateA = (a, b) => {
+  return a + b;
+};
+
+const calculateB = (a, b) => {
+  return a + b;
+};
+
+const calculateC = (a, b) => {
+  return a + b;
+};
+
 module.exports = calculate;
