f435bb8632cc7e9dbe396bca75012a12bc0dfc08
paulusmahardika <paulusmahardika@refactory.id>
2024-02-27 18:26:33 +0700

    Update CI

--- index.js
+++ index.js
@@ -2,16 +2,4 @@ const calculate = (a, b) => {
   return a + b;
 };
 
-const calculateA = (a, b) => {
-  return a + b;
-};
-
-const calculateB = (a, b) => {
-  return a + b;
-};
-
-const calculateC = (a, b) => {
-  return a + b;
-};
-
 module.exports = calculate;
